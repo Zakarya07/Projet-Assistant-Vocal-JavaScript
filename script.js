68657c717491575ec7c4fbb03fad769372b219b5
Zakarya <zakarya.elkhattabi@gmail.com>
2020-08-04 22:42:50 +0200

    Modiication du chemin vers JS => (auparavant= /script.js // dorénavant= script.js) & indentation légère du code JS

--- script.js
+++ script.js
@@ -6,9 +6,7 @@ const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecogni
 
 const hello= ["Bonjour","Salut à vous", "Héi", "Hellow !", "How are you ?","Salut !", "Salut bébé !", "Hey bèiby!"]
 
-const greetings = [`Je vais bien merci`,
- 'Je suis trés gentil',
-  'Merci de me demander, et vous ?', 'Gaulmon de banlieu'];
+const greetings = [`Je vais bien merci`, 'Je suis trés gentil','Merci de me demander, et vous ?', 'Gaulmon de banlieu'];
 
 const weather = [`Pour aujourd'hui le soleil sera au rendez-vous`,
  'Il pleut, il pleut bergère rentrez les moutons,rentrez les moutons, non sérieusement, prévoyez un parapluie ',
@@ -22,16 +20,15 @@ const lightSpeed = [`La vitesse de la lumière dans le vide, communément notée
  La lumière est l'élément le plus rapide de l'univers, pour aller de la terre à la lune il faudrait à peine 1.2 seconde pour
   la lumière pour y parvenir`];
 
-  const testingProject = 
-  ['vous avez dit test pour voir si je fonctionner correctement.. , ne vous inquiétez pas je marche trés bien grâce à vous !']
+  const testingProject =['vous avez dit test pour voir si je fonctionner correctement.., je marche trés bien grâce à vous !'];
 
-  
+
 
 
 const recognition = new SpeechRecognition();
 
 recognition.onstart = function () {
-    console.log(`Voice is Activated , you can speak to microphone`);
+    console.log(`L'écoute est activé , vous pouvez parler au microphone`);
 
 }
 
@@ -40,7 +37,6 @@ recognition.onresult = function (event) {
     const transcript = event.results[current][0].transcript;
     content.textContent = transcript;
     readOutLoud(transcript);
-
 }
 
 
@@ -59,46 +55,40 @@ function readOutLoud(message) {
     if(message.includes('comment vas-tu')||message.includes('tu vas bien') ){
        const answerForGreetings= greetings[Math.floor(Math.random()*greetings.length)];
        speech.text= answerForGreetings;
-    
     }
+
     else if (message.includes(`quel temps`) ||message.includes('quel sera la météo')|| message.includes('quelle sera la météo'))
-{
-    const answerForWeather = weather[Math.floor(Math.random()*weather.length)];
-    speech.text= answerForWeather;
-}
+    {
+        const answerForWeather = weather[Math.floor(Math.random()*weather.length)];
+        speech.text= answerForWeather;
+    }
 
+    else if (message.includes('Quel est')||message.includes('quelle est')|| message.includes('quel est')|| message.includes(`Qu'est ce que`))
+    {
+        const answerForLightSpeed = lightSpeed[Math.floor(Math.random()*lightSpeed.length)];
+        speech.text =answerForLightSpeed;
+    }
 
-else if (message.includes('Quel est')||message.includes('quelle est')|| message.includes('quel est')|| message.includes(`Qu'est ce que`)){
-    const answerForLightSpeed = lightSpeed[Math.floor(Math.random()*lightSpeed.length)];
-    speech.text =answerForLightSpeed;
+    else if (message.includes('test')){
+        const answerForTest = testingProject[Math.floor(Math.random()*testingProject.length)];
+        speech.text =answerForTest;
+    }
 
-}
+    else if (message.includes('bonjour')||message.includes('Bonjour')||message.includes('salut')||message.includes('Salut')||
+        message.includes('hey')||message.includes('Hey')||message.includes('hey kena')||message.includes('Hey Kena')||
+        message.includes('Salut Kenna')||message.includes('salut kenna')||message.includes('coucou'))
+    {
+        const answerForHello = hello[Math.floor(Math.random()*hello.length)];
+        speech.text =answerForHello;
+    }
 
-
-
-else if (message.includes('test')){
-    const answerForTest = testingProject[Math.floor(Math.random()*testingProject.length)];
-    speech.text =answerForTest;
-
-}
- else if (message.includes('bonjour')||message.includes('Bonjour')||message.includes('salut')||message.includes('Salut')||
- message.includes('hey')||message.includes('Hey')||message.includes('hey kena')||message.includes('Hey Kena')||
- message.includes('Salut Kenna')||message.includes('salut kenna')||message.includes('coucou')){
-const answerForHello = hello[Math.floor(Math.random()*hello.length)];
-speech.text =answerForHello;
- }
-
-
-else{
-    speech.text = `Désolé, je ne comprends pas`;
-
-}
-
-
+    else{
+        speech.text = `Désolé, je ne comprends pas, veuillez reformuler`;
+    }
 
 
     speech.volume = 1;
-    speech.rate = 2.3;
+    speech.rate = 1;
     speech.pitch=0.2;
 
     window.speechSynthesis.speak(speech);
