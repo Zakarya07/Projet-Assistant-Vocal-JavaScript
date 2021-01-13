58d61a8d31f51c4b6bc203d5a3f0f868f25b0d0d
Zakarya <zakarya.elkhattabi@gmail.com>
2021-01-13 01:18:59 +0100

    Ajout du fichier style.scss && crétion du dossier CSS && animation du Témoin découte (boule tournante style SIRI) lors de l'écoute de Kena

--- script.js
+++ script.js
@@ -1,37 +1,39 @@
+// Varaibles that grabs some HTML elements 
 const btn = document.querySelector('.talk');
 const content = document.querySelector(".content");
+const working = document.querySelector("#working");
 
+// Constant variable for the Recognition 
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
-
+//  Arrays that contains Answer Of Kena, not dynamic...
 const hello= ["Bonjour","Salut à vous", "Héi", "Hellow !", "How are you ?","Salut !", "Salut bébé !", "Hey bèiby!"]
-
 const greetings = [`Je vais bien merci`, 'Je suis trés gentil','Merci de me demander, et vous ?', 'Gaulmon de banlieu'];
-
 const weather = [`Pour aujourd'hui le soleil sera au rendez-vous`,
  'Il pleut, il pleut bergère rentrez les moutons,rentrez les moutons, non sérieusement, prévoyez un parapluie ',
 'Il neige', 'il pleut', 'il fait soleil', 'la température actuel est de 20° celsius', 'la température actuel est de 33° celsius',
 'la température actuel est de 6° celsius'];
-
 const lightSpeed = [`La vitesse de la lumière dans le vide, communément notée c pour «célérité» est une constante physique
  universelle et un invariant relativiste, importante dans de nombreux domaines de la physique. Sa valeur exacte est 299 792 458 m/s
   (environ 3 × 108 m/s ou 300 000 km/s). Selon la relativité restreinte, la vitesse
  de la lumière dans le vide est la vitesse maximale que peuvent atteindre toutes formes de matière ou d'information dans l'univers. 
  La lumière est l'élément le plus rapide de l'univers, pour aller de la terre à la lune il faudrait à peine 1.2 seconde pour
   la lumière pour y parvenir`];
+const testingProject =['vous avez dit test pour voir si je fonctionner correctement.., je marche trés bien grâce à vous !'];
 
-  const testingProject =['vous avez dit test pour voir si je fonctionner correctement.., je marche trés bien grâce à vous !'];
-
-
-
-
+// New instance Of SpeechRecognition Class
 const recognition = new SpeechRecognition();
 
+// Verify that the recongnition is working 
 recognition.onstart = function () {
-    console.log(`L'écoute est activé , vous pouvez parler au microphone`);
-
+    working.style="display:flex;";
+    working.classList.add("working_animation_turning");
 }
 
+// When the recording is Done, make the Rotating Indicator (that telling you that is recording) disappear
+recognition.onend = ()=> working.style="display:none;";
+ 
+// Do something with the result that we got from the user sentence(s)
 recognition.onresult = function (event) {
     const current = event.resultIndex;
     const transcript = event.results[current][0].transcript;
@@ -39,19 +41,16 @@ recognition.onresult = function (event) {
     readOutLoud(transcript);
 }
 
-
 //  add the listener to the  button
-
 
 btn.addEventListener('click', () => {
     recognition.start();
-    
 });
 
 
+//Function that make Kena "Speak" with us, depend on what the user said and if the sentence of the user contains some defined Word (see above Arrays)
 function readOutLoud(message) {
     const speech = new SpeechSynthesisUtterance();
-
 
     if(message.includes('comment vas-tu')||message.includes('tu vas bien') ){
        const answerForGreetings= greetings[Math.floor(Math.random()*greetings.length)];
@@ -84,15 +83,14 @@ function readOutLoud(message) {
     }
 
     else{
-        speech.text = `Désolé, je ne comprends pas, veuillez reformuler`;
+        speech.text = `Désolé, je ne comprends pas, veuillez reformuler.`;
     }
 
-
+    // Some settings for Kena's Voice
     speech.volume = 1;
-    speech.rate = 2;
-    speech.pitch=0.2;
+    speech.rate = 1.5;
+    speech.pitch=8;
     
-
     window.speechSynthesis.speak(speech);
     console.log(speech.text);
 
